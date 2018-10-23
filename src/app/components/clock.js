da1c990520008df2c564516460acf55469df19fb
Santiago Del Valle <sdelvalle57@gmail.com>
2018-10-23 15:04:25 +1000

    adding semantic ui functionality

--- src/app/components/clock.js
+++ src/app/components/clock.js
@@ -1,21 +1,12 @@
+import {Container} from 'semantic-ui-react';
+
 export default ({ lastUpdate, light }) => {
   return (
-    <div className={light ? 'light' : ''}>
-      {format(new Date(lastUpdate))}
-      <style jsx>{`
-        div {
-          padding: 15px;
-          display: inline-block;
-          color: #82FA58;
-          font: 50px menlo, monaco, monospace;
-          background-color: #000;
-        }
 
-        .light {
-          background-color: #999;
-        }
-      `}</style>
-    </div>
+    <Container className= {light ? 'light clock' : 'clock'}>
+      {format(new Date(lastUpdate))}
+      
+    </Container>
   )
 }
 
